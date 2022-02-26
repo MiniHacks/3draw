274b26008eb9cea36fec66de34e19c5de0a9d072
Samyok Nepal <samyok@fiveable.me>
2022-02-25 23:11:26 -0600

    cleanup

--- examples/js/spawn-in-circle.component.js
+++ examples/js/spawn-in-circle.component.js
@@ -1,33 +1,31 @@
-/* global AFRAME, THREE */
-AFRAME.registerComponent('spawn-in-circle', {
-  schema: {
-    radius: {type: 'number', default: 1}
-  },
+/* global AFRAME THREE */
+AFRAME.registerComponent("spawn-in-circle", {
+    schema: {
+        radius: { type: "number", default: 1 },
+    },
 
-  init: function() {
-    var el = this.el;
-    var center = el.getAttribute('position');
+    init: function () {
+        const el = this.el;
+        const center = el.getAttribute("position");
 
-    var angleRad = this.getRandomAngleInRadians();
-    var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
-    var worldPoint = {x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z};
-    el.setAttribute('position', worldPoint);
-    // console.log('world point', worldPoint);
+        let angleRad = this.getRandomAngleInRadians();
+        const circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
+        const worldPoint = { x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z };
+        el.setAttribute("position", worldPoint);
 
-    var angleDeg = angleRad * 180 / Math.PI;
-    var angleToCenter = -1 * angleDeg + 90;
-    angleRad = THREE.Math.degToRad(angleToCenter);
-    el.object3D.rotation.set(0, angleRad, 0);
-    // console.log('angle deg', angleDeg);
-  },
+        const angleDeg = (angleRad * 180) / Math.PI;
+        const angleToCenter = -1 * angleDeg + 90;
+        angleRad = THREE.Math.degToRad(angleToCenter);
+        el.object3D.rotation.set(0, angleRad, 0);
+    },
 
-  getRandomAngleInRadians: function() {
-    return Math.random()*Math.PI*2;
-  },
+    getRandomAngleInRadians: function () {
+        return Math.random() * Math.PI * 2;
+    },
 
-  randomPointOnCircle: function (radius, angleRad) {
-    var x = Math.cos(angleRad)*radius;
-    var y = Math.sin(angleRad)*radius;
-    return {x: x, y: y};
-  }
+    randomPointOnCircle: function (radius, angleRad) {
+        const x = Math.cos(angleRad) * radius;
+        const y = Math.sin(angleRad) * radius;
+        return { x, y };
+    },
 });
--- server/easyrtc-server.js
+++ server/easyrtc-server.js
@@ -1,75 +1,67 @@
-// Load required modules
-const http = require("http");                 // http server core module
+/* eslint-disable @typescript-eslint/no-var-requires */
+const http = require("http");
 const path = require("path");
-const express = require("express");           // web framework external module
-const socketIo = require("socket.io");        // web socket external module
-const easyrtc = require("open-easyrtc");      // EasyRTC external module
-
-// Set process name
-process.title = "networked-aframe-server";
-
-// Get port or default to 8080
+const express = require("express");
+const socketIo = require("socket.io");
+const easyrtc = require("open-easyrtc");
+process.title = "NAF-server";
 const port = process.env.PORT || 8080;
-
-// Setup and configure Express http server.
 const app = express();
 app.use(express.static(path.resolve(__dirname, "..", "examples")));
 
-// Serve the example and build the bundle in development.
+// Serve and build the bundle in development.
 if (process.env.NODE_ENV === "development") {
-  const webpackMiddleware = require("webpack-dev-middleware");
-  const webpack = require("webpack");
-  const config = require("../webpack.config");
+    const webpackMiddleware = require("webpack-dev-middleware");
+    const webpack = require("webpack");
+    const config = require("../webpack.config");
 
-  app.use(
-    webpackMiddleware(webpack(config), {
-      publicPath: "/"
-    })
-  );
+    app.use(
+        webpackMiddleware(webpack(config), {
+            publicPath: "/",
+        })
+    );
 }
 
-// Start Express http server
 const webServer = http.createServer(app);
 
-// Start Socket.io so it attaches itself to Express server
-const socketServer = socketIo.listen(webServer, {"log level": 1});
-const myIceServers = [
-  {"urls":"stun:stun1.l.google.com:19302"},
-  {"urls":"stun:stun2.l.google.com:19302"},
-  // {
-  //   "urls":"turn:[ADDRESS]:[PORT]",
-  //   "username":"[USERNAME]",
-  //   "credential":"[CREDENTIAL]"
-  // },
-  // {
-  //   "urls":"turn:[ADDRESS]:[PORT][?transport=tcp]",
-  //   "username":"[USERNAME]",
-  //   "credential":"[CREDENTIAL]"
-  // }
-];
+// Start socket.io so it attaches itself to Express server
+const socketServer = socketIo.listen(webServer, { "log level": 1 });
+const myIceServers = [{ urls: "stun:stun1.l.google.com:19302" }, { urls: "stun:stun2.l.google.com:19302" }];
 easyrtc.setOption("appIceServers", myIceServers);
 easyrtc.setOption("logLevel", "debug");
 easyrtc.setOption("demosEnable", false);
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
 easyrtc.events.on("easyrtcAuth", (socket, easyrtcid, msg, socketCallback, callback) => {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, (err, connectionObj) => {
-        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+    easyrtc.events.defaultListeners.easyrtcAuth(
+        socket,
+        easyrtcid,
+        msg,
+        socketCallback,
+        (err, connectionObj) => {
+            if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+                callback(err, connectionObj);
+                return;
+            }
+
+            connectionObj.setField("credential", msg.msgData.credential, { isShared: false });
+
+            console.log(
+                "[" + easyrtcid + "] Credential saved!",
+                connectionObj.getFieldValueSync("credential")
+            );
+
             callback(err, connectionObj);
-            return;
         }
-
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
-
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
-
-        callback(err, connectionObj);
-    });
+    );
 });
 
 // To test, lets print the credential to the console for every room join!
 easyrtc.events.on("roomJoin", (connectionObj, roomName, roomParameter, callback) => {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+    console.log(
+        "[" + connectionObj.getEasyrtcid() + "] Credential retrieved!",
+        connectionObj.getFieldValueSync("credential")
+    );
     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
@@ -80,11 +72,17 @@ easyrtc.listen(app, socketServer, null, (err, rtcRef) => {
     rtcRef.events.on("roomCreate", (appObj, creatorConnectionObj, roomName, roomOptions, callback) => {
         console.log("roomCreate fired! Trying to create: " + roomName);
 
-        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
+        appObj.events.defaultListeners.roomCreate(
+            appObj,
+            creatorConnectionObj,
+            roomName,
+            roomOptions,
+            callback
+        );
     });
 });
 
 // Listen on port
 webServer.listen(port, () => {
-    console.log("listening on http://localhost:" + port);
+    console.log("listening on http://0.0.0.0:" + port);
 });
