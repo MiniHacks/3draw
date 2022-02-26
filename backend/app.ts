d39397e0da371a1522b9cdc2f2640284d4099fa2
Ritik Mishra <ritik.mishra314@gmail.com>
2022-02-25 19:51:40 -0600

    add typescript, express, socketio, nodemon

---
+++ backend/app.ts
@@ -0,0 +1,39 @@
+import { config } from "dotenv"
+config()
+
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" },
+    path: "/express/socket.io",
+});
+
+const PORT = parseInt(process.env.PORT || "3001");
+
+app.get("/", (req, res) => {
+    res.send("hello world :)");
+});
+
+io.on("connection", (socket) => {
+    console.log({ id: socket.id, event: "connection" });
+    socket.emit("message", "Connected to socket!");
+    socket.emit("message", "ID: " + socket.id);
+    socket.on("disconnect", (reason) => {
+        console.log({ id: socket.id, event: "disconnect", reason });
+    });
+    socket.on("disconnecting", (reason) => {
+        console.log({ id: socket.id, event: "disconnecting", reason });
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+});
