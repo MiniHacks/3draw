6b04c5d857f87d9086cc8109d0ee3720ef8ca984
iCalculated <fufa0001@gmail.com>
2022-02-26 08:27:46 -0600

    recognition parameters

--- static/js/speech-recognition.js
+++ static/js/speech-recognition.js
@@ -8,6 +8,14 @@ const SpeechGrammarList = window.SpeechRecognitionGrammarList;
 const recognition = new SpeechRecognition();
 const speechRecognitionList = new SpeechGrammarList();
 
+recognition.lang = "en-US";
+recognition.continuous = true;
+recognition.interimResults = true; // half-baked answers are best
+recognition.maxAlternatives = 1;
 
 recognition.start();
 console.log("Initiated recognition.");
+
+recognition.onresult = (e) => {
+    console.log(e.results[0][0].transcript);
+};
