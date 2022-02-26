1c8a281fe0d15751141548653aa194a42b49a65f
theraggedflgyon <benweiner27@gmail.com>
2022-02-26 15:44:07 -0600

    need help debugging

---
+++ static/js/spawn-oculus.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("spawn-oculus", {
+    init: function () {
+        const self = this;
+        console.log("Listening for oculus spawns!");
+        this.el.addEventListener("click", function (event) {
+            // console.log(event);
+            // const box = document.querySelector("#boxy");
+            // box.setAttribute("material", "color", "yellow");
+            const entity = document.createElement("a-box");
+            console.log(event.detail);
+            entity.setAttribute("position", { x: 0, y: 0, z: Math.floor(Math.random() * 20) });
+            self.el.sceneEl.appendChild(entity);
+        });
+    },
+});
