5d60e53166215adba7bc4afbe9729b9797fa203c
theraggedflgyon <benweiner27@gmail.com>
2022-02-26 08:20:40 -0600

    Click events

---
+++ static/js/block.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("scale-on-mouseenter", {
+    schema: {
+        to: { default: "2.5 2.5 2.5", type: "vec3" },
+    },
+
+    init: function () {
+        console.log("Hello there!");
+        var data = this.data;
+        var el = this.el;
+        this.el.addEventListener("mouseenter", function () {
+            el.object3D.scale.copy(data.to);
+        });
+    },
+});
