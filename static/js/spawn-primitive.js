346ecd58c2289e89f384e5753ff9d8ba6952e7e4
minizou <gh.mini.zou@gmail.com>
2022-02-26 08:52:28 -0600

    add: block spawning

---
+++ static/js/spawn-primitive.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+AFRAME.registerComponent("spawn-primitive", {
+    // Init lifecycle method fires upon initialization of component.
+    init: function () {
+        let self = this;
+        console.log("spawn-primitive loads! hooray");
+
+        self.el.addEventListener("click", function (e) {
+            // point clicked (intersection point)
+            console.log(e.detail.intersection.point);
+
+            // creates box entity
+            var entity = document.createElement("a-box");
+
+            // puts the box at the point clicked
+            entity.setAttribute("position", e.detail.intersection.point);
+
+            // adds the box to the scene
+            self.el.sceneEl.appendChild(entity);
+        });
+    },
+});
