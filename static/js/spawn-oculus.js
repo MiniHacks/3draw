a4401ee0e7e5b868dff0007e48651a66ab4429ad
theraggedflgyon <benweiner27@gmail.com>
2022-02-26 15:47:28 -0600

    why the hell is the event null

--- static/js/spawn-oculus.js
+++ static/js/spawn-oculus.js
@@ -7,7 +7,7 @@ AFRAME.registerComponent("spawn-oculus", {
             // const box = document.querySelector("#boxy");
             // box.setAttribute("material", "color", "yellow");
             const entity = document.createElement("a-box");
-            console.log(event.detail);
+            console.log(event);
             entity.setAttribute("position", { x: 0, y: 0, z: Math.floor(Math.random() * 20) });
             self.el.sceneEl.appendChild(entity);
         });
