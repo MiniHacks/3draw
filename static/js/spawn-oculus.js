a4a35a86d5ee0aa64b3fa48501e16a013e383852
theraggedflgyon <benweiner27@gmail.com>
2022-02-26 16:04:43 -0600

    Looks for intersection with plane

--- static/js/spawn-oculus.js
+++ static/js/spawn-oculus.js
@@ -1,35 +1,34 @@
 AFRAME.registerComponent("raycaster-listen", {
     init: function () {
-        // Use events to figure out what raycaster is listening so we don't have to
-        // hardcode the raycaster.
         this.el.addEventListener("raycaster-intersected", (evt) => {
             this.raycaster = evt.detail.el;
         });
         this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
             this.raycaster = null;
         });
-        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const self = this;
         this.el.addEventListener("gripdown", function (event) {
-            // console.log(event);
-            // const box = document.querySelector("#boxy");
-            // box.setAttribute("material", "color", "yellow");
             const entity = document.createElement("a-box");
             console.log(self.intersectionPoint);
-            entity.setAttribute("position", { x: 0, y: 0, z: Math.floor(Math.random() * 20) });
+            if (self.intersectionPoint) {
+                entity.setAttribute("position", self.intersectionPoint);
+            }
             self.el.sceneEl.appendChild(entity);
         });
     },
 
     tick: function () {
         if (!this.raycaster) {
+            this.intersectionPoint = null;
             return;
-        } // Not intersecting.
+        } // checks for null
+        const plane = document.querySelector("#plane");
 
-        let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+        let intersection = this.raycaster.components.raycaster.getIntersection(plane);
         if (!intersection) {
+            this.intersectionPoint = null;
             return;
-        }
+        } // checks if there is no intersection
         this.intersectionPoint = intersection.point;
     },
 });
