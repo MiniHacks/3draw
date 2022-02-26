6e7b4a9e255c8d3aaf6a3b5d4fd4c39b0ae3dbbc
Samyok Nepal <samyok@fiveable.me>
2022-02-25 20:36:19 -0600

    prettier

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,10 @@
 import { config } from "dotenv";
-config();
-
 import express from "express";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
+
+config();
 
 const app = express();
 app.use(cors());
@@ -13,28 +13,29 @@ app.use(express.static("static"));
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" },
-  path: "/express/socket.io",
+    cors: { origin: "*" },
+    path: "/express/socket.io",
 });
 
 const PORT = parseInt(process.env.PORT || "3001");
 
 app.get("/", (req, res) => {
-  res.send("hello world :)");
+    res.send("hello world :)");
 });
 
 io.on("connection", (socket) => {
-  console.log({ id: socket.id, event: "connection" });
-  socket.emit("message", "Connected to socket!");
-  socket.emit("message", "ID: " + socket.id);
-  socket.on("disconnect", (reason) => {
-    console.log({ id: socket.id, event: "disconnect", reason });
-  });
-  socket.on("disconnecting", (reason) => {
-    console.log({ id: socket.id, event: "disconnecting", reason });
-  });
+    console.log({ id: socket.id, event: "connection" });
+    socket.emit("message", "Connected to socket!");
+    socket.emit("message", "ID: " + socket.id);
+    socket.on("disconnect", (reason) => {
+        console.log({ id: socket.id, event: "disconnect", reason });
+    });
+
+    socket.on("disconnecting", (reason) => {
+        console.log({ id: socket.id, event: "disconnecting", reason });
+    });
 });
 
 server.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}`);
+    console.log(`listening on http://localhost:${PORT}`);
 });
